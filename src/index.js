3657e3583986092786ecb040fc0698b7d75eba3f
t3roja <ronkainen@hotmail.fi>
2024-11-10 15:50:53 +0200

    Register ja Login toimii

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
+import Home from './screens/Home'
+import Authentication, { AuthenticationMode } from './screens/Authentication'
+import ErrorPage from './screens/ErrorPage'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import UserProvider from './context/UserProvider'
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {errorElement: <ErrorPage />
+
+  },
+  {
+    path: "/signin",
+    element: <Authentication authenticationMode={AuthenticationMode.Login}/>,
+  },
+  {
+    path: '/signup',
+    element: <Authentication authenticationMode={AuthenticationMode.Register}/>
+  },
+  {
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: '/',
+        element: <Home/>,
+      }
+    ]
+  }
+])
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+        <RouterProvider router={router} />
+    </UserProvider>
   </React.StrictMode>
 );
 
